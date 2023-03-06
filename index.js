514cee9b15c9605e41325d6c840d074672d0072c
colecrum26 <cwcrumrine@gmail.com>
2023-03-06 14:23:13 -0600

    playing with vanilla js

---
+++ index.js
@@ -0,0 +1,26 @@
+// PARAMETERS AND ARGUMENTS - the way I remember this is P.A.
+
+// KEYWORDS ARE THINGS AUTOMATICALLY RECOGNIZED BY THE CODE AT RUNTIME
+
+// var something = "hello world";
+
+// console.log(something)
+
+// function cookies(){
+//     console.log("hello world")
+//     console.log(12345678)
+// }
+
+// cookies();
+var args = "hello bananas";
+var pizza = "mama mia"
+var nums = 12345;
+
+function bananas(params){
+    console.log(params)
+}
+
+
+bananas(pizza);
+bananas(args);
+bananas(nums)
