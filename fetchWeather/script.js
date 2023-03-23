5068178389677f6641dae031a82035abe224cd78
colecrum26 <cwcrumrine@gmail.com>
2023-03-23 15:01:00 -0500

    setting up OpenWeather API

---
+++ fetchWeather/script.js
@@ -0,0 +1,10 @@
+const apiKey = "5bfff3ebda0e0482eab8cdd3efecce1a";
+const cityName = "new+orleans";
+const state = "LA";
+const countryCode = "US";
+const limit = "5";
+const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${state},${countryCode}&limit=${limit}&appid=${apiKey}`;
+
+function getCityWeather(){
+    fetch()
+}
