d1d56f10ba7fbcd812654b130c9ac8bebaa69eea
colecrum26 <cwcrumrine@gmail.com>
2023-03-23 10:21:23 -0500

    set up reject

--- promises.js
+++ promises.js
@@ -2,5 +2,10 @@ let promise = new Promise((resolve, reject)=>{
     setTimeout(()=>{
         let num = Math.round(Math.random()*10);
         console.log(num);
-    },3000)
+        if (num % 2 == 0){
+            resolve(num);
+        } else {
+            reject(num);
+        }
+    },1000)
 });
