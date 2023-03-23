f4d40e9b99bae1140b51f330eabeb6f9c4d1ecea
colecrum26 <cwcrumrine@gmail.com>
2023-03-23 10:31:08 -0500

    using .then

--- promises.js
+++ promises.js
@@ -1,11 +1,22 @@
-let promise = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        let num = Math.round(Math.random()*10);
-        console.log(num);
-        if (num % 2 == 0){
-            resolve(num);
-        } else {
-            reject(num);
-        }
-    },1000)
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let num = Math.round(Math.random() * 10);
+    console.log(num);
+    if (num % 2 == 0) {
+      resolve(num);
+    } else {
+      reject(num);
+    }
+  }, 1000);
 });
+
+promise
+  .then((x) => {
+    return { x, doubled: x * 3 };
+  })
+  .then((obj) => {
+    console.log(`${obj.x} doubled is ${obj.doubled}`);
+  })
+  .catch((err) => {
+    console.error(`${err} the number is odd`);
+  });
