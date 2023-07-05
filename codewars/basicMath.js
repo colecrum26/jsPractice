6c7d0149944927dfea2ac798fb85bdd0715469a6
colecrum26 <cwcrumrine@gmail.com>
2023-07-05 13:21:11 -0400

    testing codewars

---
+++ codewars/basicMath.js
@@ -0,0 +1,9 @@
+let operation = '+';
+let value1 = 4;
+let value2 = 7;
+
+function basicMath(operation, value1, value2) {
+return operation === '+' ? value1 + value2 : 'NaN';
+}
+
+console.log(basicMath(operation, value1, value2));
