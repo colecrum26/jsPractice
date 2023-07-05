4990451378a264a1d6878aac9d542d7d7d355262
colecrum26 <cwcrumrine@gmail.com>
2023-07-05 13:23:25 -0400

    comments

--- codewars/basicMath.js
+++ codewars/basicMath.js
@@ -2,8 +2,13 @@ let operation = '+';
 let value1 = 4;
 let value2 = 7;
 
+// Switch statements require a return statement to perform an operation
+
 function basicMath(operation, value1, value2) {
-return operation === '+' ? value1 + value2 : 'NaN';
+    switch(operation) {
+        case '+':
+            return value1 + value2;
+    }
 }
 
 console.log(basicMath(operation, value1, value2));
