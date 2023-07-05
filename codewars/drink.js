2bf3b8bb39b93a7cfbb824b304958a658f1d5984
colecrum26 <cwcrumrine@gmail.com>
2023-07-05 16:03:40 -0400

    testing codewars

---
+++ codewars/drink.js
@@ -0,0 +1,9 @@
+let age = 22;
+
+function drink(age) {
+    return age < 14 ? 'drink toddy' :
+            14 >= age && age < 18 ? 'drink coke':
+              18 >= age && age < 21 ? 'drink beer': 'drink whisky';
+  };
+
+console.log(drink(age));
