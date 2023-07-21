f12901230d2656b9828b8baf6b574a9afecf15d9
colecrum26 <cwcrumrine@gmail.com>
2023-07-21 09:56:46 -0500

    testing a file

---
+++ practice/oop.js
@@ -0,0 +1,11 @@
+const person = {
+    "_id": 1,
+    "first_name": "Leslie",
+    "last_name": "Yepp",
+    "cell": "8125552344",
+    "city": "Pawnee",
+    "hobbies": ["scrapbooking", "eating waffles", "working"]
+ };
+
+ console.log(person.id);
+ 
