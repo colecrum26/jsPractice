ed8c98e0a8cf12eddeea7b5d1a296bb9f43ae864
colecrum26 <cwcrumrine@gmail.com>
2023-09-03 18:12:27 -0500

    various solutions

--- codewars/sumPos.js
+++ codewars/sumPos.js
@@ -1,14 +1,29 @@
 let arr = [1, 2, 3, 4, 5];
 
 function positiveSum(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; ++i) {
-      if (arr[i] > 0) {
-         sum += arr[i];
-      } else {
-        return 0;
-      } return sum;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    } else {
+      return 0;
     }
   }
+  return sum;
+}
+
+// function positiveSum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       sum += arr[i];
+//     }
+//   }
+//   return sum;
+// }
+
+// function positiveSum(arr) {
+//     return arr.reduce((a, b) => a > 0 ? a + b : 0);
+// }
 
 console.log(positiveSum(arr));
