09e58a4db39f9ab03d834f728d6370cb97c0de91
colecrum26 <cwcrumrine@gmail.com>
2023-09-06 18:08:01 -0500

    further testing using regex

--- codewars/pangram.js
+++ codewars/pangram.js
@@ -1,7 +1,10 @@
-let string = "The quick brown fox jumps over the lazy dog.";
+let string = "This is not a pangram.";
 
 function isPangram(string) {
-    return string.toLowerCase().match(/\w/g) ? true : false;
+    return string.match(/([a-z])(?!.*\1)/gi).length === 26 ? true : false;
   }
+
+//   var regex = /([a-z])(?!.*\1)/gi;
+//   return (str.match(regex) || []).length === 26
 
 console.log(isPangram(string));
